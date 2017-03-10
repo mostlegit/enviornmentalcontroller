70cbef0a6f4d1334174e88a4c1e6654f43c78e8c
Chase S <chases@inmotionhosting.com>
2017-03-10 16:34:57 -0700

    arduino json

--- Arduino/get_ph/get_ph.ino
+++ Arduino/get_ph/get_ph.ino
@@ -14,10 +14,11 @@ outputValue = map(sensorValue, 0, 1023, 0, 14);
 // change the analog out value:
 analogWrite(analogOutPin, outputValue);
 // print the results to the serial monitor:
-Serial.print("sensor = " );
+Serial.print("{\"sensor\" : ");
 Serial.print(sensorValue);
-Serial.print("\t output = ");
-Serial.println(analogRead(2)* 14.0 / 1024, 1);
+Serial.print(" \t \"PH\" : ");
+Serial.print(analogRead(2)* 14.0 / 1024, 1);
+Serial.println("}");
 // wait 10 milliseconds before the next loop
 // for the analog-to-digital converter to settle
 // after the last reading:
